cc2d0e588ca9a9c2d1b236b6c63a6379580a7a2d
root <root@web4.softgalia.com>
2014-02-06 13:02:22 +0100

    iniciamos el proyecto web4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Webdream4::Application.config.secret_key_base = 'fb09b5d3639314fc9f5f0285645f7cef1c7850ba22f17b485d72ea3415bfeee36ad89f7b595e0054f1687fea6ab0ad5b77745c4098fa1aac8c29fa713a6e7be9'
