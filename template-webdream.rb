5fa485bb9caee1e5da6ee00807c77ef4aaef4f9d
root <root@web4.softgalia.com>
2014-02-19 19:08:25 +0100

    vesion0001

--- template-webdream.rb
+++ template-webdream.rb
@@ -1,10 +1,8 @@
-class Newgem < Thor::Group
-      include Thor::Actions
-
-        # Define arguments and options
-         argument :name
-         class_option :test_framework, :default => :test_unit
-
+class mytemplate < Thor
+  include Thor::Actions
+   #Define arguments and options
+   argument :name
+   class_option :test_framework, :default => :test_unit
 
   def self.source_root
       File.dirname(__FILE__)
@@ -12,13 +10,28 @@ class Newgem < Thor::Group
 
   def instalar_config
     if yes?("Quieres instalar la configuracion")
-        copy_file  "config/modelo ", "app/models/config"
-
+        copy_file  "config/modelo", "app/models/config"
+        copy_file  "config/config_controller", "app/controllers/config_controller"
+        directory  "view/", "app/view/config_view.html"
     else
-        say "Shame on you…", :re
+       say("Sin config entonces!!", GREEN)
     end
   end
 
+  def instalar_dc-config
+  end
+
+  def instalar_blog
+  end
+
+  def instalar_devise
+  end
+
+  def instalar_categorias
+  end
+
+  def instalar_articulos
+  end
 
 
 end
