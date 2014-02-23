324e86bce4fb0f32e831aefcefe432f46b952400
softgalia <michon@web4.softgalia.com>
2014-02-23 18:07:22 +0100

    Ya creamos la app

--- template-webdream.rb
+++ template-webdream.rb
@@ -1,38 +1,15 @@
-#plantilla webdream
-class mytemplate < Thor
-  include Thor::Actions
-   #Define arguments and options
-   argument :name
-   class_option :test_framework, :default => :test_unit
+# GEMas
 
-  def self.source_root
-      File.dirname(__FILE__)
-  end
+gem 'execjs'
+gem 'therubyracer'
 
-  def instalar_config
-    if yes?("Quieres instalar la configuracion")
-        copy_file  "config/modelo", "app/models/config"
-        copy_file  "config/config_controller", "app/controllers/config_controller"
-        directory  "view/", "app/view/config_view.html"
-    else
-       say("Sin config entonces!!", GREEN)
+    # cambios en config para que apunte a la base de datos del cliente
+    # cambios en el fichero inflector para que pluralize correctametne
+    inject_into_file 'config/initializers/inflections.rb', before: "# These inflection rules are supported but not enabled by default:\n" do <<-'CODE'
+
+      ActiveSupport::Inflector.inflections do |inflect|
+         inflect.irregular 'configuracion', 'configuraciones'
+      end
+      CODE
     end
-  end
-
-  def instalar_dc-config
-  end
-
-  def instalar_blog
-  end
-
-  def instalar_devise
-  end
-
-  def instalar_categorias
-  end
-
-  def instalar_articulos
-  end
-
-
-end
+    generate "scaffold", "configuracion empnomcom:string empnomsoc:string"
